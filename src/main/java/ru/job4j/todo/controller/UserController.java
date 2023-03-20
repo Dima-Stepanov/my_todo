9ca54068da554067ab07031dc1431db313860770
dmitryjob4j <haoos@inbox.ru>
2023-03-20 12:18:51 +0300

    controller and view Users

--- src/main/java/ru/job4j/todo/controller/UserController.java
+++ src/main/java/ru/job4j/todo/controller/UserController.java
@@ -2,8 +2,16 @@ package ru.job4j.todo.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.todo.model.User;
 import ru.job4j.todo.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 3. Мидл
@@ -21,4 +29,49 @@ import ru.job4j.todo.service.UserService;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping("/login")
+    public String getLogin() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute User user,
+                            Model model,
+                            HttpServletRequest request) {
+        var userOptional = userService.findUserByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        var session = request.getSession();
+        session.setAttribute("user", userOptional.get());
+        return "redirect:/";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(@ModelAttribute User user,
+                               Model model,
+                               HttpServletRequest request) {
+        var userOptional = userService.create(user);
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Пользователь с email:" + user.getEmail() + " зарегистрирован");
+            return "users/register";
+        }
+        var session = request.getSession();
+        session.setAttribute("user", userOptional.get());
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/register")
+    public String gerRegister() {
+        return "users/register";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/users/login";
+    }
 }
---
+++ src/main/java/ru/job4j/todo/filter/AuthorizationFilter.java
@@ -0,0 +1,47 @@
+package ru.job4j.todo.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 3. Мидл
+ * 3.3. HibernateТема
+ * 3.3.2. Конфигурирование
+ * 4. TODO list
+ * AuthorizationFilter HttpFilter order(1) фильтр авторизации в приложении.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 20.03.2023
+ */
+@Component
+@Order(1)
+public class AuthorizationFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request,
+                            HttpServletResponse response,
+                            FilterChain chain) throws IOException, ServletException {
+        var uri = request.getRequestURI();
+        if (isAlwaysPermitted(uri)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        var userLoggedIn = request.getSession().getAttribute("user") != null;
+        if (!userLoggedIn) {
+            var loginPageUrl = request.getContextPath() + "/users/login";
+            response.sendRedirect(loginPageUrl);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean isAlwaysPermitted(String uri) {
+        return uri.startsWith("/users/register") || uri.startsWith("/users/login");
+    }
+}
